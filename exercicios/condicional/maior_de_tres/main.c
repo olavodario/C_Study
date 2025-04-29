32f24de6caab734d8e08021d1985b9477750001a
Olavo Dario <olavoribgodario@gmail.com>
2025-04-29 07:27:11 -0300

    feat: implementa solução para encontrar o maior de três números
    Implementa um programa em C que lê três números inteiros do usuário e imprime o maior deles.  O programa inclui uma função `limpar_entrada()` para lidar com possíveis problemas de entrada.

---
+++ exercicios/condicional/maior_de_tres/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main() {
+    
+    int n1, n2, n3;
+
+    printf("Primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Segundo numero: ");
+    scanf("%d", &n2);
+    printf("Terceiro numero: ");
+    scanf("%d", &n3);
+
+    int maior;
+
+    maior = n1;
+
+    if (n2 > n1 && n2 > n3) {
+        maior = n2;
+    }
+    else if (n3 > n2 && n3 > n1){
+        maior = n3;
+    }
+}
